78ac9fc2a0f355521d4dd7a98b042ef95c731e16
Haris-115 <hsuhail32@gmail.com>
2021-08-11 20:07:31 +0500

    changes in users db

---
+++ ManagementSystemAPI/ManagementSystemAPI/Common/Constant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManagementSystemAPI.Common
+{
+    public class Constant
+    {
+        public enum DetailType
+        {
+            distributors,
+            manufactures,
+            medstores
+        }
+    }
+}
